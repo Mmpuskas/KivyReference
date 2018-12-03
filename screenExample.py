642ff1c6209b454eda661c2eb46f1dd41a25d4a8
Michael Puskas <mmpuskas@gmail.com>
2018-12-02 17:57:50 -0700

    Added extra page with a progress bar.

--- screenExample.py
+++ screenExample.py
@@ -1,18 +1,19 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.progressbar import ProgressBar
 
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
 # property manager that gives you the instance of the ScreenManager used.
 Builder.load_string("""
-<MenuScreen>:
+<StartScreen>:
     AnchorLayout:
         anchor_x: 'center'
         anchor_y: 'center'
 
         Label:
-            text: 'Menu'
+            text: 'Start'
 
     AnchorLayout:
         anchor_x: 'right'
@@ -23,7 +24,7 @@ Builder.load_string("""
             text: 'Next'
             on_press:
                 root.manager.transition.direction = 'left'
-                root.manager.current = 'settings'
+                root.manager.current = 'progress'
             size: 100, 50
             size_hint: None, None
 
@@ -37,13 +38,56 @@ Builder.load_string("""
             size: 100, 50
             size_hint: None, None
 
-<SettingsScreen>:
+<ProgressScreen>:
+    AnchorLayout:
+        anchor_x: 'center'
+        anchor_y: 'center'
+        padding: 50, 0, 50, 0
+
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint_y: None
+            spacing: 20
+
+            Label:
+                text: 'Updating...'
+
+            ProgressBar:
+                value: 50
+
+    AnchorLayout:
+        anchor_x: 'right'
+        anchor_y: 'bottom'
+        padding: 0, 0, 10, 10
+
+        Button:
+            text: 'Next'
+            on_press:
+                root.manager.transition.direction = 'left'
+                root.manager.current = 'done'
+            size: 100, 50
+            size_hint: None, None
+
+    AnchorLayout:
+        anchor_x: 'left'
+        anchor_y: 'bottom'
+        padding: 10, 0, 0, 10
+
+        Button:
+            text: 'Back'
+            on_press:
+                root.manager.transition.direction = 'right'
+                root.manager.current = 'start'
+            size: 100, 50
+            size_hint: None, None
+
+<DoneScreen>:
     AnchorLayout:
         anchor_x: 'center'
         anchor_y: 'center'
 
         Label:
-            text: 'Settings'
+            text: 'Done'
 
     AnchorLayout:
         anchor_x: 'right'
@@ -64,22 +108,26 @@ Builder.load_string("""
             text: 'Back'
             on_press:
                 root.manager.transition.direction = 'right'
-                root.manager.current = 'menu'
+                root.manager.current = 'progress'
             size: 100, 50
             size_hint: None, None
 """)
 
 # Declare both screens
-class MenuScreen(Screen):
+class StartScreen(Screen):
     pass
 
-class SettingsScreen(Screen):
+class ProgressScreen(Screen):
+    pass
+
+class DoneScreen(Screen):
     pass
 
 # Create the screen manager
 sm = ScreenManager()
-sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(SettingsScreen(name='settings'))
+sm.add_widget(StartScreen(name='start'))
+sm.add_widget(ProgressScreen(name='progress'))
+sm.add_widget(DoneScreen(name='done'))
 
 class TestApp(App):
 
